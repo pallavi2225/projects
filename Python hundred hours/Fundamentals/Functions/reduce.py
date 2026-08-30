4bf613ca3f9e4baa536e309032be0e033122118a
pallavi2225 <pallavit7008@gmail.com>
2026-08-30 16:18:06 +0530

    recursion

--- Python hundred hours/Fundamentals/Functions/reduce.py
+++ Python hundred hours/Fundamentals/Functions/reduce.py
@@ -2,3 +2,8 @@ from functools import reduce
 numbers=[1,2,3,4,5]
 result=reduce(lambda a, b: a+b, numbers)
 print(result)
+
+#reduce() → one final result
+nos=[10,30,56,78,45]
+largest=reduce(lambda a,b: a if a > b else b,nos)
+print(largest)
