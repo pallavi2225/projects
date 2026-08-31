e2a144bb7fed5e7462bf9446cf6bbb2c351863c2
pallavi2225 <pallavit7008@gmail.com>
2026-08-31 15:50:57 +0530

    docstring

---
+++ Python hundred hours/Fundamentals/Functions/docString.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    """ Returns the sum of two numbers"""  #The text inside triple quotes is documentation.
+    return a + b
+
+
+#access it
+
+print(add.__doc__)
+
+def calculate_total(pricess):
+    """Calculate and returns the total price"""
+    return sum(pricess)
