494cbbd8a98fe731415ae81dc21cd0e79bceb410
pallavi2225 <pallavit7008@gmail.com>
2026-08-31 22:32:49 +0530

    Decorators

---
+++ Python hundred hours/Fundamentals/Functions/decorators.py
@@ -0,0 +1,20 @@
+# Decorators are functions that add extra behavior to another function.
+def decorator_function(function):
+    def wrapper():
+        print("Before function")
+        function()
+        print("after function")
+    return wrapper
+
+def say_hello():
+    print("hello")
+
+say_hello= decorator_function(say_hello)
+say_hello()
+
+def say_hi():
+    print("hi")
+
+say_hi = decorator_function(say_hi)
+say_hi()
+
