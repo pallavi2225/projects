43c51af844dda998b05529540d89186a865efeda
pallavi2225 <pallavit7008@gmail.com>
2026-09-03 16:29:22 +0530

    All function

--- Python hundred hours/Fundamentals/Functions/any.py
+++ Python hundred hours/Fundamentals/Functions/any.py
@@ -1,3 +1,4 @@
+#any() = Does at least one satisfy the condition? then true.
 numbers=[1,3,5]
 result=any(num % 2 == 0 for num in numbers)
 print(result)
@@ -5,3 +6,18 @@ print(result)
 no=[2,4,1,3]
 re=any(num % 2 ==0  for num in no)
 print(re)
+
+
+print("======All=====")
+#all() Every condition must be True. 
+no=[2,4,8,10]
+result=all(nu % 2 == 0 for nu in no)
+print(result)
+# True output because all numbers are even.
+
+numb=[1,3,4,5]
+re=all(n % 2==0 for n in numb)
+print(re)
+
+
+
